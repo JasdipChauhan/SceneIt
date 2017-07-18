daa3c602026bc084e3a3cef46fc3e787b5a3863c
JasdipC <jasdip.chauhan@gmail.com>
2017-07-17 21:58:50 -0400

    added devise user model

--- sceneit-backend/config/routes.rb
+++ sceneit-backend/config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_constraints.rb'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  #API definition
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      #list resources here
+    end
+  end
 end
