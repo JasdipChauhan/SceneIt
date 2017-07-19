c0293455cff7c7db57929e64fec462f9c26a4290
JasdipC <jasdip.chauhan@gmail.com>
2017-07-18 23:18:30 -0400

    added user auth and exposing it as an endpoint

--- sceneit-backend/README.md
+++ sceneit-backend/README.md
@@ -1,5 +1,9 @@
 # README
 
+<!-- Helper functions -->
+curl -H 'Accept: application/vnd.sceneit.v1' \ http://api.sceneit.dev/users/1
+<!--  -->
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- sceneit-backend/spec/models/user_spec.rb
+++ sceneit-backend/spec/models/user_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+end
+
+
+describe User do
+  before { @user = FactoryGirl.build(:user) }
+
+  subject { @user }
+
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { shoud validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
 end
