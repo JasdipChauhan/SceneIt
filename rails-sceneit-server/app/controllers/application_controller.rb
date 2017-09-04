9b0d6832acea2a291574f793eb2566050d3d2592
JasdipC <jasdip.chauhan@gmail.com>
2017-09-03 20:35:46 -0400

    deleting deprecated rails backend API

--- rails-sceneit-server/app/controllers/application_controller.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationController < ActionController::API
-  include ActionController::RequestForgeryProtection
-  protect_from_forgery with: :null_session
-
-  include Authenticable
-end
--- sceneIt-app/SceneIt/PostTableViewController.swift
+++ sceneIt-app/SceneIt/PostTableViewController.swift
@@ -20,6 +20,7 @@ class PostTableViewController: UITableViewController {
             }
             
             self.postList = postList!
+            self.tableView.reloadData()
         })
 
         // Uncomment the following line to preserve selection between presentations
@@ -38,24 +39,25 @@ class PostTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return postList.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "postCell", for: indexPath)
+        cell.textLabel?.text = postList[indexPath.row].title
         return cell
     }
-    */
 
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
